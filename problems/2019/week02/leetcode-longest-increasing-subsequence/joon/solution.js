f6f0120dd5cba5598ba7f337cb283846b31d9248
wintersalmon <salmonjoon@gmail.com>
2019-01-12 20:03:49 +0900

    Submit 2019/week02/leetcode-longest-increasing-subsequence

--- problems/2019/week02/leetcode-longest-increasing-subsequence/joon/solution.js
+++ problems/2019/week02/leetcode-longest-increasing-subsequence/joon/solution.js
@@ -4,5 +4,26 @@
  */
 // eslint-disable-next-line no-unused-vars
 function lengthOfLIS(nums) {
-    return 0;
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    const cachedLengths = new Array(nums.length);
+    cachedLengths[0] = 1;
+    let maxLength = 1;
+
+    for (let iCurrent = 1; iCurrent < nums.length; iCurrent += 1) {
+        let previousMaxLength = 0;
+
+        for (let iPrevious = 0; iPrevious < iCurrent; iPrevious += 1) {
+            if (nums[iCurrent] > nums[iPrevious]) {
+                previousMaxLength = Math.max(previousMaxLength, cachedLengths[iPrevious]);
+            }
+        }
+
+        cachedLengths[iCurrent] = previousMaxLength + 1;
+        maxLength = Math.max(maxLength, cachedLengths[iCurrent]);
+    }
+
+    return maxLength;
 }
